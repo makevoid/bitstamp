cc2eb076dc499b181de4685eef8bdc08cd357b2a
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-02-05 13:45:06 +0000

    add nonce for gets
    from https://github.com/nubis/bitstamp/commit/8a946b9057fa753ae7f6b50178cf4c1e449dd083

--- lib/bitstamp/net.rb
+++ lib/bitstamp/net.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 module Bitstamp
   module Net
     def self.to_uri(path)
@@ -5,7 +7,7 @@ module Bitstamp
     end
 
     def self.get(path, options={})
-      RestClient.get(self.to_uri(path))
+      RestClient.get(self.to_uri(path) + "?#{SecureRandom.urlsafe_base64(5)}")
     end
 
     def self.post(path, options={})
